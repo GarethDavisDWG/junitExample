ad843e7a3640cfffd05025b4eb591d324f2446dc
Gareth <gdavis@softwareinstitute.com>
2021-10-15 09:51:31 +0100

    unit testing example code

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        Pen pen1    = new Pen("Blue","Fine");
+        Pen pen2 = new Pen("Red","Fat");
+
+        System.out.println(pen1);
+    }
+
+}
